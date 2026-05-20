fbc1db5843c59cb1c9433d347483b306e0e4a057
MarkoSt0 <markostevic2003@gmail.com>
2026-05-20 20:30:33 +0200

    docs: dodati javadoc komentari za model Sertifikat

--- src/main/java/rs/ac/bg/fon/izdavanjestanovaback/model/Sertifikat.java
+++ src/main/java/rs/ac/bg/fon/izdavanjestanovaback/model/Sertifikat.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rs.ac.bg.fon.izdavanjestanovaback.model;
 
 import jakarta.persistence.*;
@@ -10,7 +6,11 @@ import java.util.Date;
 import lombok.*;
 
 /**
- *
+ * Predstavlja domensku klasu za strucni sertifikat koju agent poseduje.
+ * Klasa je mapirana na tabelu "sertifikat" u bazi podataka.
+ * Omogucava pracenje kvalifikacija i strucnosti agenata unutar sistema.
+ * Podrzava kreiranje objekata preko Lombok Builder sablona.
+ * 
  * @author Marko
  */
 @Entity
@@ -20,21 +20,41 @@ import lombok.*;
 @Builder
 @Data
 public class Sertifikat implements Serializable{
+    
+    /**
+     * Jedinstveni identifikator sertifikata u bazi podataka (Primarni kljuc).
+     * Generise se automatski na strani baze koriscenjem IDENTITY strategije.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long idSertifikat;
     
+    /**
+     * Zvanicni naziv sertifikata ili polozene obuke (npr. Sertifikat za posrednika u prometu nekretnina).
+     */
     @Column
     private String nazivSertifikata;
     
+    /**
+     * Pravno lice, institucija ili organizacija koja je izdala sertifikat (npr. Privredna komora Srbije).
+     */
     @Column
     private String izdavalac;
     
+    /**
+     * Datum kada je agent zvanicno stekao sertifikat.
+     * U bazi se cuva iskljucivo kao cist datum (dan, mesec, godina).
+     */
     @Temporal(TemporalType.DATE)
     @Column
     private Date datumSticanja;
     
+    /**
+     * Agent koji poseduje ovaj konkretni sertifikat.
+     * Predstavlja spoljni kljuc (ManyTOOne veza) ka tabeli agent.
+     * Podaci o agentu se ucitavaju odmah (EAGER) zajedno sa sertifikatom.
+     */
     @JoinColumn(name = "idAgent", referencedColumnName = "id")
     @ManyToOne(fetch = FetchType.EAGER)
     private Agent agent; 
