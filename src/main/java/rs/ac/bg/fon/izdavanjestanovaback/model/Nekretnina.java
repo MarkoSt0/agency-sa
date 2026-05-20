6741d4c9f667e1322eb25ef744d240f4c01db0f2
MarkoSt0 <markostevic2003@gmail.com>
2026-05-20 20:26:29 +0200

    docs: dodati javadoc komentari za model Nekretnina

--- src/main/java/rs/ac/bg/fon/izdavanjestanovaback/model/Nekretnina.java
+++ src/main/java/rs/ac/bg/fon/izdavanjestanovaback/model/Nekretnina.java
@@ -4,11 +4,16 @@ import java.io.Serializable;
 import java.util.Collection;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import rs.ac.bg.fon.izdavanjestanovaback.enums.StatusNekretnine;
 import rs.ac.bg.fon.izdavanjestanovaback.enums.TipGrejanja;
 
 /**
- *
+ * Predstavlja domensku klasu za nekretninu koja je predmet izdavanja (npr. stan, kuca, garaza).
+ * Klasa je mapirana na tabelu "nekretnina" u bazi podataka.
+ * Sadrzi fizicke karakteristike objekta, lokaciju, status dostupnosti, 
+ * kao i relacije ka tipu nekretnine i stavkama ugovora u kojima se pojavljuje.
+ * 
  * @author Marko
  */
 @Entity
@@ -23,45 +28,89 @@ import rs.ac.bg.fon.izdavanjestanovaback.enums.TipGrejanja;
     @NamedQuery(name = "Nekretnina.findByGodinaIzgradnje", query = "SELECT n FROM Nekretnina n WHERE n.godinaIzgradnje = :godinaIzgradnje"),
     @NamedQuery(name = "Nekretnina.findByOpis", query = "SELECT n FROM Nekretnina n WHERE n.opis = :opis")})
 @Data
+@NoArgsConstructor
 public class Nekretnina implements Serializable {
 
+    /**
+     * Identifikacioni broj za serijalizaciju objekta.
+     */
     private static final long serialVersionUID = 1L;
+    
+    /**
+     * Jedinstveni identifikator nekretnine u bazi podataka (Primarni kljuc).
+     * Generise se automatski koriscenjem IDENTITY strategije.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "idNekretnina")
     private Long idNekretnina;
+    
+    /**
+     * Adresa na kojoj se nekretnina nalazi (ulica, broj, grad).
+     */
     @Column(name = "adresa")
     private String adresa;
+    
+    /**
+     * Kvadratura (povrsina) nekretnine izrazena u kvadratnim metrima.
+     */
     @Column(name = "povrsina")
     private String povrsina;
+    
+    /**
+     * Sprat na kojem se nekretnina nalazi (npr. Prizemlje, 4. sprat, Potkrovlje).
+     */
     @Column(name = "sprat")
     private String sprat;
-    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    
+    /**
+     * Ukupan broj soba u nekretnini (npr. 1.5, 2.0, 3.5).
+     */
     @Column(name = "brojSoba")
     private Double brojSoba;
+    
+    /**
+     * Godina izgradnje objekta u kojem se nekretnina nalazi.
+     */
     @Column(name = "godinaIzgradnje")
     private Integer godinaIzgradnje;
+    
+    /**
+     * Dodatni tekstualni opis nekretnine (stanje, opremljenost, prednosti, nedostaci i dr.).
+     */
     @Column(name = "opis")
     private String opis;
+    
+    /**
+     * Kolekcija svih stavki izdavanja u kojima se ova nekretnina pojavljuje.
+     * Podaci se ucitavaju odmah (EAGER) prilikom ucitavanja same nekretnine.
+     */
     @OneToMany(mappedBy = "idNekretnina", fetch = FetchType.EAGER)
     private Collection<StavkaIzdavanja> stavkaIzdavanjaCollection;
+    
+    /**
+     * Kategorija ili tip kojem nekretnina pripada (npr. Stan, Kuca).
+     * Predstavlja spoljni kljuc (ManyToOne veza) ka klasi TipNekretnine.
+     */
     @JoinColumn(name = "idTipaNekretnine", referencedColumnName = "idTipaNekretnine")
     @ManyToOne
     private TipNekretnine tipNekretnine;
     
+    /**
+     * Vrsta grejanja koja je instalirana u nekretnini.
+     * Cuva se u bazi podataka kao tekstualna vrednost (String) odgovarajuceg enuma.
+     */
     @Enumerated(EnumType.STRING)
     @JoinColumn(name = "tipGrejanja")
     private TipGrejanja tipGrejanja;
     
+    /**
+     * Trenutni status nekretnine na trzistu (npr. Slobodna, Izdata, Rezervisana).
+     * Cuva se u bazi podataka kao tekstualna vrednost (String) odgovarajuceg enuma.
+     */
     @Enumerated(EnumType.STRING)
     @JoinColumn(name = "statusNekretnine")
     private StatusNekretnine statusNekretnine;
 
-    public Nekretnina() {
-    }
-
-    public Nekretnina(Long idNekretnina) {
-        this.idNekretnina = idNekretnina;
-    }
 }
