d84ddfcb55a8dcbb3de87ba16f7185a3eac64444
MarkoSt0 <markostevic2003@gmail.com>
2026-05-27 17:00:40 +0200

    feat: dodata jakarta validacija i javadoc za klasu Izdavanje

--- src/main/java/rs/ac/bg/fon/izdavanjestanovaback/model/Izdavanje.java
+++ src/main/java/rs/ac/bg/fon/izdavanjestanovaback/model/Izdavanje.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -47,39 +52,57 @@ public class Izdavanje implements Serializable {
     /**
      * Datum kada je ugovor o izdavanju zvanicno sklopljen i potpisan.
      * U bazi se cuva bez vremenske komponente (samo dan, mesec i godina).
+     * Polje je obavezno i mora biti danasnji datum ili datum u proslosti.
      */
+    @NotNull(message = "Datum sklapanja ugovora je obavezan.")
+    @PastOrPresent(message = "Datum sklapanja ugovora mora biti danasnji datum ili datum u proslosti.")
     @Column(name = "datumSklapanjaUgovora")
     @Temporal(TemporalType.DATE)
     private Date datumSklapanjaUgovora;
     
     /**
      * Trenutni status ugovora o najmu (npr. Aktivan, Istekao, Storniran).
+     * Polje je obavezno, ne sme biti null niti sadrzati samo razmake.
+     * Maksimalna duzina je 255 karaktera.
      */
+    @NotBlank(message = "Status ugovora je obavezan.")
+    @Size(max = 255, message = "Status ugovora ne sme biti duzi od 255 karaktera.")
     @Column(name = "statusUgovora")
     private String statusUgovora;
     
     /**
      * Dogovoreni nacin placanja zakupnine (npr. Mesecno, Kes, Racun, Unapred).
+     * Polje je obavezno, ne sme biti null niti sadrzati samo razmake.
+     * Maksimalna duzina je 255 karaktera.
      */
+    @NotBlank(message = "Nacin placanja je obavezan.")
+    @Size(max = 255, message = "Nacin placanja ne sme biti duzi od 255 karaktera.")
     @Column(name = "nacinPlacanja")
     private String nacinPlacanja;
     
     /**
      * Ukupna finansijska vrednost ugovora o izdavanju izrazena kroz decimalni broj.
+     * Polje je obavezno i ne sme biti negativan broj.
      */
+    @NotNull(message = "Ukupan iznos mora biti naveden.")
+    @Min(value = 0, message = "Ukupan iznos ne sme biti negativan broj.")
     @Column(name = "ukupanIznos")
     private Double ukupanIznos;
     
     /**
      * Dodatne napomene ili specificni uslovi vezani za realizaciju izdavanja.
+     * Polje je opcionalno. Maksimalna duzina je 255 karaktera.
      */
+    @Size(max = 255, message = "Napomena ne sme biti duza od 255 karaktera.")
     @Column(name = "napomena")
     private String napomena;
     
     /**
      * Agent koji je posredovao u realizaciji ovog izdavanja i kreirao ugovor.
      * Predstavlja spoljni kljuc (ManyToOne veza) koji referencira primarni kljuc klase Agent.
+     * POlje je obavezno.
      */
+    @NotNull(message = "Agent mora biti izabran.")
     @JoinColumn(name = "idAgent", referencedColumnName = "id")
     @ManyToOne
     private Agent idAgent;
@@ -87,7 +110,9 @@ public class Izdavanje implements Serializable {
     /**
      * Klijent koji se pojavljuje u ulozi zakupca (onaj koji iznajmljuje nekretninu).
      * Predstavlja spoljni kljuc (ManyToOne veza) koji referencira primarni kljuc klase Klijent.
-     */
+     * POlje je obavezno.
+     */ 
+    @NotNull(message = "Klijent mora biti izabran.")
     @JoinColumn(name = "idKlijent", referencedColumnName = "id")
     @ManyToOne
     private Klijent idKlijent;
